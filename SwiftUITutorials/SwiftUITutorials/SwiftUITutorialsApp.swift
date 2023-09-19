cdac9d4d11ffd5f7e7eeaf159498e9b387d19226
wormsJJG <81068345+wormsJJG@users.noreply.github.com>
2023-09-19 10:34:00 +0900

    Project Setting

---
+++ SwiftUITutorials/SwiftUITutorials/SwiftUITutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialsApp.swift
+//  SwiftUITutorials
+//
+//  Created by 정재근 on 2023/09/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
